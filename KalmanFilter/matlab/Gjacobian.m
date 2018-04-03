94318af987ee63f0efc79c1a4bfbd7d13d9a47a1
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2018-04-03 22:12:02 +0200

    deleted

--- KalmanFilter/matlab/Gjacobian.m
+++
@@ -1,3 +0,0 @@
-function G = Gjacobian(u)
-c = cos(u(3)); s = sin(u(3));
-G = [0,0,0;0,0,0;c, -s, -u(1)*s-u(2)*c; s, c, u(1)*c-u(2)*s];
--- KalmanFilter/matlab/gfunc.m
+++
@@ -1,3 +0,0 @@
-function g = gfunc(u)
-c = cos(u(3)); s = sin(u(3));
-g = [0; 0; c*u(1)-s*u(2); s*u(1)+c*u(2)];
